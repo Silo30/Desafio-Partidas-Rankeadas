208ec146068c11065ba9b5a1b61e0cf5d16e27ba
Silo30 <silomenezes3010@gmail.com>
2023-12-28 20:03:25 -0300

    correção de condições

--- rankeada.js
+++ rankeada.js
@@ -1,4 +1,4 @@
-let rankDoHeroi = calcularRank(200,190)
+let rankDoHeroi = calcularRank(200,150)
 let nivelDoHeroi;
 
 function calcularRank(vitorias, derrotas){
@@ -8,8 +8,18 @@ function calcularRank(vitorias, derrotas){
 
 if (rankDoHeroi <= 10){
     nivelDoHeroi = "ferro"
-} else if (rankDoHeroi <=20){
+} else if (rankDoHeroi === 11 || rankDoHeroi <= 20){
     nivelDoHeroi = "bronze"
+} else if (rankDoHeroi  === 21 || rankDoHeroi <=50){
+    nivelDoHeroi = "prata"
+} else if (rankDoHeroi  === 51 || rankDoHeroi <=80){
+    nivelDoHeroi = "ouro"
+} else if (rankDoHeroi  === 51 || rankDoHeroi <=80){
+    nivelDoHeroi = "diamante"
+} else if (rankDoHeroi  === 81 || rankDoHeroi <=100){
+    nivelDoHeroi = "Lendário"
+} else {
+    nivelDoHeroi = "Imortal"
 }
 
 console.log("O Herói tem de saldo " + rankDoHeroi + " vitórias e está no nível de " + nivelDoHeroi);
