fbf07ba1c236c424c372e9acac9dbd4e21886b9a
Silo30 <silomenezes3010@gmail.com>
2023-12-28 19:51:08 -0300

    ranks 1

--- rankeada.js
+++ rankeada.js
@@ -1,8 +1,15 @@
-let rankDoHeroi = calcularRank(200,140)
+let rankDoHeroi = calcularRank(200,190)
+let nivelDoHeroi;
 
 function calcularRank(vitorias, derrotas){
     let saldoRankeadas = vitorias - derrotas;
     return saldoRankeadas
 }
 
-console.log(rankDoHeroi)
+if (rankDoHeroi <= 10){
+    nivelDoHeroi = "ferro"
+} else if (rankDoHeroi <=20){
+    nivelDoHeroi = "bronze"
+}
+
+console.log("O Herói tem de saldo " + rankDoHeroi + "vitórias");
